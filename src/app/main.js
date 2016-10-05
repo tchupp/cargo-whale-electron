959cce30f94a276a9256ef7ca708999a1c11a0f3
tclchiam <tclchiam@gmail.com>
2016-10-04 23:52:15 -0400

    Added hash location support

--- src/app/main.js
+++ src/app/main.js
@@ -1,4 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core'
+import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {Application} from 'app/app';
 
-bootstrap(Application);
+bootstrap(Application, [
+  ROUTER_PROVIDERS,
+  provide(APP_BASE_HREF, {useValue: '/'}),
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
+]);
